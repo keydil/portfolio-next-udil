4430c6026d86d9f53f154d80a89f087be82281c6
keydil <fadhilfirdausadha493@gmail.com>
2025-07-10 21:41:17 +0700

    fix: update release date

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,11 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-black text-white p-4">
       <h1 className="text-4xl font-bold mb-4 text-center">
         <SparklesText>
-          Portfolio Coming Soon
+          Portfolio Fadhil Coming Soon
         </SparklesText>
       </h1>
       <p className="text-lg text-center">
-        Akan rilis maksimal tanggal <strong>10 Juli, jam 22.00 WIB</strong>
+        Akan rilis maksimal tanggal <strong>11 Juli, jam 22.00 WIB</strong>
       </p>
       <p className="text-sm text-center mt-4">
         Stay tuned! Karya terbaik sedang dipoles 🛠️✨
